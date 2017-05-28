17e50ecdf918a0810aff7942dcfb43d7b794caa0
arid1995 <arid1995@mail.ru>
2017-05-28 15:13:04 +0300

    Merged

---
+++ dispatching system backend/src/main/java/ru/bmstu/rk9/mechanics/commands/messages/Message.java
@@ -0,0 +1,28 @@
+package ru.bmstu.rk9.mechanics.commands.messages;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Date;
+
+/**
+ * Created by farid on 4/7/17.
+ */
+
+public abstract class Message {
+    public long getTimestamp(){
+        long timestamp = System.currentTimeMillis()/1000;
+        return  timestamp;
+    }
+
+    private static final ObjectMapper message = new ObjectMapper();
+
+    public String toJson() {
+        try {
+            return message.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- dispatching system backend/src/main/java/ru/bmstu/rk9/mechanics/models/Stacker.java
+++ dispatching system backend/src/main/java/ru/bmstu/rk9/mechanics/models/Stacker.java
@@ -1,5 +1,7 @@
 package ru.bmstu.rk9.mechanics.models;
 
+import ru.bmstu.rk9.mechanics.commands.ConveyorCommand;
+
 /**
  * Created by farid on 5/2/17.
  */
@@ -11,7 +13,7 @@ public class Stacker extends Device {
 
   public void putPalletOnConveyor(Pallet pallet) {
     pallet.removeFromOrder();
-    sendMessageToDevice();
+    sendMessageToDevice(new ConveyorCommand());
   }
 
   public void takePalletFromConveyor(Billet billet) {
